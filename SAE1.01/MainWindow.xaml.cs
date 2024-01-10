189e1ec7c61efa137cc24d1d470b1b7ca748b5bb
Baptiste Crepon <Baptiste.Crepon@etu.univ-smb.fr>
2024-01-10 11:10:16 +0100

    Apparition Nouvel Ennemi v1

--- SAE1.01/MainWindow.xaml.cs
+++ SAE1.01/MainWindow.xaml.cs
@@ -22,8 +22,9 @@ namespace SAE1._01
     public partial class MainWindow : Window
     {
         private DispatcherTimer dispatcherTimer = new DispatcherTimer();
-        private double vitesseEnnemi = 2;
+        private double vitesseEnnemi = 0.5;
         private ImageBrush ennemiSkin = new ImageBrush();
+        private bool apparitonNouvelEnnemi = false;
         public MainWindow()
         {
             InitializeComponent();
@@ -34,7 +35,7 @@ namespace SAE1._01
             // Lancement timer
             dispatcherTimer.Start();
             CreationEnnemi();
-            ennemiSkin.ImageSource = new BitmapImage(new Uri(AppDomain.CurrentDomain.BaseDirectory + "img/AV2.png"));
+            ennemiSkin.ImageSource = new BitmapImage(new Uri(AppDomain.CurrentDomain.BaseDirectory + "img/A.png"));
         }
         private void CanvasKeyIsDown(object sender, KeyEventArgs e)
         { }
@@ -53,18 +54,20 @@ namespace SAE1._01
             };
             // Placement aléatoire
             Canvas.SetTop(nouvelEnenemi, 0);
-            Canvas.SetLeft(nouvelEnenemi, random.Next(30, (int)Application.Current.MainWindow.Width-29));
+            Canvas.SetLeft(nouvelEnenemi, random.Next(30, (int)Application.Current.MainWindow.Width-(29+(int)nouvelEnenemi.Width)));
             myCanvas.Children.Add(nouvelEnenemi);
             // Modification vitesse pour accélérer
             vitesseEnnemi += 0.1;
         }
         private void DeplacementEnnemi(Rectangle ennemi)
         {
-            // Deplacement vers le bas
+            // Deplacement vers le bas 
             Canvas.SetTop(ennemi, Canvas.GetTop(ennemi) + vitesseEnnemi);
         }
         private void Jeu(object sender, EventArgs e)
         {
+            Random random = new Random();
+
             // Parcourt de tous les rectangles du canvas
             foreach (var y in myCanvas.Children.OfType<Rectangle>())
             {
@@ -73,7 +76,20 @@ namespace SAE1._01
                 {
                     // Deplacement
                     DeplacementEnnemi(y);
+
+                    // Detectiion hauteur pour apparation nouvel ennemi
+                    if (Canvas.GetTop(y) >= random.Next(80, 130))
+                    {
+                        apparitonNouvelEnnemi = true;
+                    }
                 }
+            }
+
+            // Test pour faire apparaitre nouvel ennemi
+            if (apparitonNouvelEnnemi)
+            {
+                CreationEnnemi();
+                apparitonNouvelEnnemi = false;
             }
         }
     }
